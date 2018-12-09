0f158c0647372d265a5c6c5407ceac1101eab70a
Chad Groft <chad@flexport.com>
2018-12-08 21:37:50 -0800

    Clean up day 7

--- day07/order_with_multiple_elves.rb
+++ day07/order_with_multiple_elves.rb
@@ -10,32 +10,29 @@ def next_step(deps)
   deps.each_pair.select { |k, v| v.empty? }.map { |k, _| k }.min
 end
 
-MAX_ELVES_WORKING = 5
 def time_for_step(step)
   step.ord - "A".ord + 61
 end
+
+MAX_ELVES_WORKING = 5
 elves_working = []
 current_time = 0
 
 until dependencies.empty? && elves_working.empty?
   step = next_step(dependencies)
   if step && elves_working.size < MAX_ELVES_WORKING
-    # assign an elf to perform a task
-    elf_record = {step: step, finished: current_time + time_for_step(step)}
-    elves_working << elf_record
+    # assign an elf to a task
+    elves_working << {step: step, finished: current_time + time_for_step(step)}
     dependencies.delete(step)
-    # puts "Assigning an elf to step #{step}, will complete at t = #{elf_record[:finished]}"
   else
     # wait for some elves to complete their tasks
     current_time = elves_working.map { |e| e[:finished] }.min
-    # puts "Waiting for t = #{current_time}"
     elves_finished_or_not = elves_working.group_by { |e| e[:finished] == current_time }
     elves_working = elves_finished_or_not[false] || []
     elves_finished_or_not[true].map { |e| e[:step] }.each do |s|
       dependencies.values.each { |deps| deps.delete(s) }
-      # puts "Completed step #{s}"
     end
   end
-  # puts "#{elves_working.size} elves working"
 end
+
 puts "Finish in #{current_time} s"
