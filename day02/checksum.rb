fb5d15978087df6c693c248b6f806f43913b183f
Chad Groft <chad@flexport.com>
2018-12-08 20:36:09 -0800

    Clean up day 2

--- day02/checksum.rb
+++ day02/checksum.rb
@@ -1,12 +1,10 @@
 twos = 0
 threes = 0
 
-File.open('day02/input.txt').each do |l|
+File.open("day02/input.txt").each_line do |l|
   counts = l.chomp.chars.group_by { |c| c }.values.map(&:size)
   twos += 1 if counts.include?(2)
   threes += 1 if counts.include?(3)
 end
 
-checksum = twos * threes
-p checksum
-
+puts "The checksum value is #{twos * threes}"
--- day02/find_ids.rb
+++ day02/find_ids.rb
@@ -1,18 +1,13 @@
-lines = File.open('day02/input.txt').map(&:chomp).map(&:chars)
+lines = File.open("day02/input.txt").each_line.map { |l| l.chomp.chars }
 
+# A bit inefficient since it compares lines to themselves and checks some pairs twice,
+# but only by a constant factor.
 lines.each do |line_1|
   lines.each do |line_2|
     diffs = line_1.zip(line_2).select { |a| a[0] != a[1] }.count
     if diffs == 1
       puts "#{line_1.zip(line_2).select { |a| a[0] == a[1] }.map { |a| a[0] }.join}\n"
+      return
     end
-  end
-end
-(1...lines.size).each do |j|
-  row_j = lines[j]
-  (0...j).each do |i|
-    row_i = lines[i]
-    diffs = row_i.zip(row_j).select { |a| a[0] != a[1] }.count
-    p "#{row_i} vs. #{row_j}" && return if diffs == 1
   end
 end
